636b0c077e77dd0e0977b1fa69a33edf021fbd5d
Gabriela-Guerra <gabrielaguerra.arq@gmail.com>
2021-05-13 19:02:01 -0300

    adiciona requisitos da aula 6.2

---
+++ 6.2/script.js
@@ -0,0 +1,7 @@
+let myModal = new bootstrap.Modal(document.getElementById('myModal'));
+let button = document.querySelector('#send');
+
+button.addEventListener('click', function() {
+    myModal.show();
+});
+
